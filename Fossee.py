ad9c2f79a37c175dc97eed84a8a233ea1af32c5e
BhaanuChauhan <48313032+BhaanuChauhan@users.noreply.github.com>
2019-03-28 17:21:03 +0530

    Update Fossee.py

--- Fossee.py
+++ Fossee.py
@@ -6,16 +6,27 @@ Created on Sat Mar 23 23:00:54 2019
 """
 
 import sys
-from PyQt4 import QtGui
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QAction
 
-class Fossee(QtGui.QMainWindow):
+class Fossee(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(Fossee, self).__init__()
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle("Graph Plot")
+        mainMenu = self.menuBar()
+        fileMenu=mainMenu.addMenu("File")
+        editMenu = mainMenu.addMenu("Edit")
+        loadData = fileMenu.addMenu("Load")
+        editData= editMenu.addMenu("Edit Data")
+        addData=fileMenu.addMenu("Add Data")
+        saveData=fileMenu.addMenu("Save as png")
+        exitButton= QAction("Exit",self)
+        exitButton.triggered.connect(self.close)
+        fileMenu.addAction(exitButton)
         self.show()
 
-app = QtGui.QApplication(sys.argv)
+app = QtWidgets.QApplication(sys.argv)
 GUI = Fossee()
 sys.exit(app.exec_())
